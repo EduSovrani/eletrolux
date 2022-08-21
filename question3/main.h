a5f8bf4925fce85d43592ff1b4922764559c2f66
Eduardo Falchetti Sovrani <edusovrani@gmail.com>
2022-08-21 19:55:14 -0300

    Final Code of the Question 3

---
+++ question3/main.h
@@ -0,0 +1,16 @@
+/**
+* @file <main.h>
+* @brief <Header File>
+*
+* <Question 3 - Eletrolux - Knowledge Assessment>
+*
+* @author <Eduardo Falchetti Sovrani>
+*
+*/
+
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+void find_statistics(int *_array, unsigned int _size, float *_result, int *_new_size, int *_result_even);
+
+#endif // MAIN_H_INCLUDED
